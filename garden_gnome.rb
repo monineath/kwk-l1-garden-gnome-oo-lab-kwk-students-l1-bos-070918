4a828eead1da68c98455a673591b511f7683ead8
monineath <monineathh@gmail.com>
2018-07-13 15:57:04 +0000

    Done.

--- garden_gnome.rb
+++ garden_gnome.rb
@@ -1,1 +1,28 @@
 # Code your instances here
+class GardenGnome
+  attr_accessor :name, :age, :gluten_allergy 
+  attr_reader :personality, :hat_color
+  
+  
+  def initialize(hat_color = "red")
+    @personality = "evil"
+    @hat_color = hat_color
+  
+  end
+  
+  def gnaw
+   "Gnawing on a tree!!!"
+  end
+  
+  def shout 
+    "GNARLY!!!"
+  end
+  
+  def introduce_self
+    "Hello humans, my name is Frankie the Frightening, I am 324 years old, and you'll rue the day you crossed me!"
+  end
+end 
+GardenGnome.new
+
+
+  
